7284800727eb98d6abc40c65130e87360a2ce770
Leandro <leandrom.roldan@gmail.com>
2022-10-20 12:20:23 -0300

    change on firebase initializeApp

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONNECTION);
 
-if (!admin.app.length) {
+if (admin.app.length == 0) {
 	admin.initializeApp({
 		credential: admin.credential.cert(serviceAccount),
 	});
