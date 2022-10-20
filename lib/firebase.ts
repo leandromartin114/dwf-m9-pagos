5077a70583ce8c47b51683c92807ee0980433d5a
Leandro <leandrom.roldan@gmail.com>
2022-10-20 12:23:35 -0300

    initializeApp

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONNECTION);
 
-if (admin.app.length == 0) {
-	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount),
-	});
-}
-// admin.initializeApp({
-// 	credential: admin.credential.cert(serviceAccount),
-// });
+// if (admin.app.length == 0) {
+// 	admin.initializeApp({
+// 		credential: admin.credential.cert(serviceAccount),
+// 	});
+// }
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
 
 export const firestore = admin.firestore();
